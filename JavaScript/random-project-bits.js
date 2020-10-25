b761847cc56a86a099ba703048e447d4d4de69a5
Dom Golby <dom.golby@gmail.com>
2020-10-25 23:07:39 +0000

    Update random-project-bits.js

--- JavaScript/random-project-bits.js
+++ JavaScript/random-project-bits.js
@@ -139,7 +139,16 @@ function truthyOrFalsy(a) {
    
    
            // ---------------------------------------------------------------------------------------
+           const fs = require('fs');
 
+           fs.readFile('./script.js', function(error, data) {
+             // error is null if no error occurred, but an Error object if it did
+             if (error) {
+              throw error;
+             }
+             // the file data will be passed into the callback if no error was thrown
+             console.log(data);
+           });
    
    
-   
+              // ---------------------------------------------------------------------------------------
