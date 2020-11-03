49b567ae07db190f8dbca3505486b918053fb53f
Dom Golby <dom.golby@gmail.com>
2020-11-03 13:39:36 +0000

    Create whale-talk.js

---
+++ JavaScript/whale-talk.js
@@ -0,0 +1,43 @@
+let input = 'a whale of a deal!';
+const vowels = ['a','e','i','o','u'];
+resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  // console.log(i);
+  if (input[i] === 'e') {
+       resultArray.push(input[i]);
+              resultArray.push(input[i]);
+            
+            // console.log(resultArray);
+
+     }
+
+     if (input[i] === 'u') {
+       resultArray.push(input[i]);
+              resultArray.push(input[i]);
+            // console.log(resultArray);
+
+     }
+          if (input[i] === 'i') {
+       resultArray.push(input[i]);
+            // console.log(resultArray);
+
+     }
+          if (input[i] === 'a') {
+       resultArray.push(input[i]);
+            // console.log(resultArray);
+
+     }
+               if (input[i] === 'o') {
+       resultArray.push(input[i]);
+            // console.log(resultArray);
+
+     }
+  for (let x = 0; x < vowels.length; x++) {
+    //  console.log(x);
+     
+
+  }
+}
+
+            console.log(resultArray.join('').toUpperCase());
